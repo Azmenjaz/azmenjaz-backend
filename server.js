acf8347a2ffd1fcb2ca35535347dd2d0f444fe89
Azmenjaz <support@azmenjaz.com>
2026-02-01 21:00:15 +0300

    Update server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors({
-  origin: ['https://azmenjaz.com', 'http://localhost:3000'],
+  origin: '*' //['https://azmenjaz.com', 'http://localhost:3000'],
   credentials: true
 }));
 app.use(express.json());
@@ -104,3 +104,4 @@ app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
   console.log(`Environment: ${process.env.NODE_ENV || 'development'}`);
 });
+
