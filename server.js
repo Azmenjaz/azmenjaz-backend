00fb1658f15164b0a4df85f09c9fc412f054c9c2
Azmenjaz <support@azmenjaz.com>
2026-02-07 23:23:54 +0300

    Update server.js

--- server.js
+++ server.js
@@ -65,7 +65,7 @@ app.post('/api/flights/search', async (req, res) => {
 
     const flights = result.flights.map(flight => ({
       ...flight,
-      bookingLink: AmadeusService.getBookingLink(flight.airlineCode, originCode, destinationCode, departureDate)
+      bookingLink: AmadeusService.getBookingLink(flight.airlineCode)
     }));
 
     res.json({ success: true, flights: flights, count: flights.length });
