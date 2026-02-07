61bbf5069f1a6fbb1466db51bca6cbb0c95ca0e4
Azmenjaz <support@azmenjaz.com>
2026-02-07 11:07:43 +0300

    Update amadeusService.js

--- services/amadeusService.js
+++ services/amadeusService.js
@@ -4,7 +4,7 @@ require('dotenv').config();
 const amadeus = new Amadeus({
   clientId: process.env.AMADEUS_CLIENT_ID,
   clientSecret: process.env.AMADEUS_CLIENT_SECRET,
-  hostname: 'test' // استخدم 'production' بعد التأكد من أن كل شيء يعمل
+  hostname: 'production' // Set to production based on usage
 });
 
 class AmadeusService {
